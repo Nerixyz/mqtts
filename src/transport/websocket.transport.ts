3e583fdd0c454161863fc62277bba228b3f8c2cf
Nerixyz <nero.9@hotmail.de>
2020-02-22 18:33:20 +0100

    feat: websocket additional options

--- src/transport/websocket.transport.ts
+++ src/transport/websocket.transport.ts
@@ -1,11 +1,11 @@
 import { Transport } from './transport';
 import WebSocket = require('ws');
 
-export class WebsocketTransport extends Transport<{ url: string }> {
+export class WebsocketTransport extends Transport<{ url: string, additionalOptions?: WebSocket.ClientOptions }> {
     private socket: WebSocket;
 
     connect(): void {
-        this.socket = new WebSocket(this.options.url);
+        this.socket = new WebSocket(this.options.url, this.options.additionalOptions);
         this.socket.on('open', () => this.callbacks.connect());
         this.socket.on('message', (data: Buffer) => this.callbacks.data(data));
         this.socket.on('close', () => this.callbacks.disconnect());
