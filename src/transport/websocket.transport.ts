fdf51c31d99dc66007a02d987ca451a0725d2bf0
Nerixyz <nero.9@hotmail.de>
2021-03-17 21:16:39 +0100

    fix: simplify stream creation

--- src/transport/websocket.transport.ts
+++ src/transport/websocket.transport.ts
@@ -3,6 +3,7 @@ import * as WebSocket from 'ws';
 import { ClientOptions } from 'ws';
 import { Duplex, PassThrough } from 'stream';
 import duplexify = require('duplexify');
+import { Duplexify } from 'duplexify';
 
 export interface WebsocketTransportOptions {
     url: string;
@@ -11,26 +12,23 @@ export interface WebsocketTransportOptions {
 
 export class WebsocketTransport extends Transport<WebsocketTransportOptions> {
     // this will be set on the constructor
-    public duplex!: Duplex;
+    public duplex!: Duplexify;
     private socket?: WebSocket;
     private socketStream?: Duplex;
-    private readonly readable = new PassThrough();
-    private readonly writable = new PassThrough();
-
     constructor(options: WebsocketTransportOptions) {
         super(options);
         this.reset();
     }
 
     reset() {
-        this.duplex = duplexify(this.writable, this.readable, { objectMode: true });
+        this.duplex = duplexify(undefined, undefined, { objectMode: true });
     }
 
     connect(): Promise<void> {
         this.socket = new WebSocket(this.options.url, this.options.additionalOptions);
         this.socketStream = WebSocket.createWebSocketStream(this.socket, { objectMode: true });
-        this.socketStream.pipe(this.readable);
-        this.writable.pipe(this.socketStream);
+        this.duplex.setReadable(this.socketStream);
+        this.duplex.setWritable(this.socketStream);
 
         return new Promise(resolve => this.socket?.on('open', resolve));
     }
